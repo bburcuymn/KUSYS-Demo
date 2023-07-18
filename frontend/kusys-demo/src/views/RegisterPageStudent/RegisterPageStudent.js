9c300f040e3a84107a8d2ed7db01a65a7786e2a3
Burcu Yaman <bburcu0003@gmail.com>
2023-07-18 12:13:58 +0300

    last edit

--- frontend/kusys-demo/src/views/RegisterPageStudent/RegisterPageStudent.js
+++ frontend/kusys-demo/src/views/RegisterPageStudent/RegisterPageStudent.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const RegisterPageAdmin = () => {
+const RegisterPageStudent = () => {
     // State tanımlamaları
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -122,4 +122,4 @@ const RegisterPageAdmin = () => {
     );
 };
 
-export default RegisterPageAdmin;
+export default RegisterPageStudent;
